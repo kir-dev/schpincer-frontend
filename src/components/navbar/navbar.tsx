8abcd73b25005655ab8615d51e3eeb6fdea78b74
David Strumpf <strumpf.david721@gmail.com>
2024-07-06 04:59:49 +0200

    fix: absolute navigation & dropdown animation

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import { useState } from 'react';
 import { FaHamburger } from 'react-icons/fa';
 
+import { Separator } from '@/components/ui/separator';
+
 const links = [
   {
     id: 1,
@@ -21,6 +23,10 @@ const links = [
     id: 4,
     link: 'contact',
   },
+  {
+    id: 5,
+    link: 'checkout',
+  },
 ];
 
 export default function Navbar() {
@@ -31,7 +37,7 @@ export default function Navbar() {
   };
 
   return (
-    <header>
+    <header className='fixed top-0 w-full'>
       <div className='flex justify-between items-center w-full h-20 px-4 text-black bg-white'>
         <div className='flex items-center justify-center'>
           <FaHamburger size={30} />
@@ -48,7 +54,7 @@ export default function Navbar() {
               key={id}
               className='nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-black duration-200 link-underline'
             >
-              <Link href={link}>{link}</Link>
+              <Link href={`/${link}`}>{link}</Link>
             </li>
           ))}
         </ul>
@@ -58,22 +64,22 @@ export default function Navbar() {
         </button>
       </div>
       {/* Dropdown menu for small screens */}
-      <div className='md:hidden bg-white'>
-        {isOpen ? (
-          <ul className='flex flex-col space-y-3 pt-3 pb-3'>
-            {links.map(({ id, link }) => (
-              <li
-                key={id}
-                className='nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:text-black duration-200 link-underline'
-              >
-                <Link href={link}>{link}</Link>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <div />
-        )}
+      <div className='md:hidden bg-white overflow-hidden'>
+        <ul
+          className='flex flex-col space-y-3 duration-300 transition-all ease-in-out'
+          style={{ height: isOpen ? 33 * links.length + 12 : 0 }}
+        >
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className='nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:text-black duration-200 link-underline'
+            >
+              <Link href={`/${link}`}>{link}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
+      <Separator />
     </header>
   );
 }
