b29a04ec08a9ca7d81814c38dd026573c6efbf9c
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-07-06 16:35:33 +0200

    feat: minor ui improvements

--- src/components/providers/provider-list-item.tsx
+++ src/components/providers/provider-list-item.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
-import { TbCalendarDue } from 'react-icons/tb';
+import Link from 'next/link';
+import { FaCalendar } from 'react-icons/fa';
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { formatHu } from '@/lib/utils';
@@ -11,33 +12,35 @@ interface ProviderProps {
 
 export function ProviderListItem({ provider }: ProviderProps) {
   return (
-    <Card
-      className='relative pt-20 text-center mt-20 hover:-translate-y-5 transition-transform cursor-pointer'
-      key={provider.id}
-    >
-      <CardHeader>
-        <CardTitle>{provider.name}</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p>{provider.description}</p>
-        <div className='flex items-center text-slate-400 gap-1 justify-center mt-5'>
-          <TbCalendarDue className='inline-block' />
-          {provider.nextOpening ? formatHu(provider.nextOpening.date) : 'Nincs nyitás dátum'}
-        </div>
-      </CardContent>
-      <div
-        className='absolute left-0 right-0 top-0 h-2 rounded-t-md'
-        style={{
-          backgroundColor: provider.color,
-        }}
-      />
-      <Image
-        src={provider.image}
-        alt={provider.name}
-        width={150}
-        height={150}
-        className='object-cover w-32 h-32 absolute -top-16 rounded-full mx-auto left-0 right-0 shadow-md'
-      />
-    </Card>
+    <Link href={`/providers/${provider.id}`}>
+      <Card
+        className='relative pt-20 text-center mt-20 hover:-translate-y-5 transition-transform cursor-pointer'
+        key={provider.id}
+      >
+        <CardHeader>
+          <CardTitle>{provider.name}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>{provider.description}</p>
+          <div className='flex items-center text-slate-400 gap-1 justify-center mt-5'>
+            <FaCalendar className='inline-block' />
+            {provider.nextOpening ? formatHu(provider.nextOpening.date) : 'Nincs nyitás dátum'}
+          </div>
+        </CardContent>
+        <div
+          className='absolute left-0 right-0 top-0 h-2 rounded-t-md'
+          style={{
+            backgroundColor: provider.color,
+          }}
+        />
+        <Image
+          src={provider.image}
+          alt={provider.name}
+          width={150}
+          height={150}
+          className='object-cover w-32 h-32 absolute -top-16 rounded-full mx-auto left-0 right-0 shadow-md'
+        />
+      </Card>
+    </Link>
   );
 }
